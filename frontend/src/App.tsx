c26e610a1fe8c88120eb137ef0219be76c024c2e
Alexian <alexian@gojob.com>
2021-11-14 13:25:23 +0100

    wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  border: none;
+  border-radius: 0.375rem;
+  padding: 0 1rem;
+  background-color: #3865fd;
+  color: white;
+  height: 2rem;
+  font-size: 16px;
+`;
+
+const Div = styled.div`
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div>
+      <Button>Salut</Button>
+    </Div>
   );
 }
 
